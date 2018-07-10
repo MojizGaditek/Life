4b541af4fca33cbdb424b711050a9617f6a35435
MojizGaditek <mojiz.gaditek@gmail.com>
2018-07-10 14:42:19 +0500

    Forgot Password changes - include class, method and function

--- ForgotPassword.py
+++ ForgotPassword.py
@@ -1,17 +1,18 @@
+import unittest
 from selenium import webdriver
 
-url = "http://devlife.gaditek-hrms.sg.plesk-server.com/"
-Email = "zahid.friend123@gmail.com"
 
-mydriver = webdriver.Firefox()
-mydriver.get(url)
+class Forgotpassword(unittest.TestCase):
+    @classmethod
+    def test_TC_2_Forgot_password(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.get("http://devlife.gaditek-hrms.sg.plesk-server.com/")
+        cls.driver.find_element_by_xpath('/html/body/section/div/div/div/form/a').click()
+        cls.driver.find_element_by_xpath("//*[@id='email']").send_keys("zahid.friend123@gmail.com")
+        cls.driver.find_element_by_xpath('//*[@id="emp_login"]').click()
+        sucess_message = cls.driver.find_element_by_xpath('/html/body/section/div/div/div/div/div/div').text
+        print(sucess_message)
 
-mydriver.find_element_by_xpath('/html/body/section/div/div/div/form/a').click()
 
-mydriver.find_element_by_xpath("//*[@id='email']").clear()
-mydriver.find_element_by_xpath("//*[@id='email']").send_keys(Email)
-mydriver.find_element_by_xpath('//*[@id="emp_login"]').click()
-
-assert mydriver.page_source.find("We have e-mailed your password reset link!")
-
-mydriver.quit()
+if __name__ == '__main__':
+    unittest.main(failfast=True, exit=False)
